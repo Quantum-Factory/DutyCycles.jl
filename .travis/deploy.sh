8621efa2960932918b1ecdd8f790c8348a66a0ca
Bjoern Stein <bjoern.stein@quantum-factory.de>
2019-04-28 14:46:50 +0200

    removed echo option

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@ echo '|1|tCbF6ylipISewwuZfzDbsw623ic=|zj/fFRcWw7BfUrYvl0pTSmSfqNQ= ecdsa-sha2-ni
 echo '|1|jKCOWAH9yrDZ3ey2kQewgzuMjvc=|tivCVhqU7PCYU6Sz6AAAPN735/c= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDypicw6GdbqBGMXgcrXoxRNUyOJWRBzv06a2TMlO7fQ34OSiekvBcJue7HLnFNYUpqlr7SHKynK8OcP8pFMntJEoDgIboWuFCphZhzEfBSjpryNF/Cp8jpF4vswi42FpbWCsooGn9NuXmW0isoCWQVEz73412gSvFnbgG/s8JSf21wlVl1yUDBI6m5btrikhGU34movU/3qQy44x/02pvBVAenTaSfH2IyjayUpr7vb0fF/inNCebmDAKitJ5vbfmVuoCYcZ0P+KSuR7XjVuFa2SbS5AJbUwKRj31tw4Fd1I1yii3XBKYm5DygkVCSIfTvu7Qi/B4nE97RymtSp5cr' >>$HOME/.ssh/known_hosts
 mv docs/build docs/DutyCycles.jl
 ssh-add $HOME/.ssh/id_rsa
-echo -e 'put -r docs/DutyCycles.jl' |sftp -P $SSH_PORT $SSH_USER@$SSH_HOST:docs/github.com/Quantum-Factory
+echo 'put -r docs/DutyCycles.jl' |sftp -P $SSH_PORT $SSH_USER@$SSH_HOST:docs/github.com/Quantum-Factory
